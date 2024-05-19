26b5201ef80ed4008356f7993254a06f53e636ee
NicolasOgloblin <nicolas.ogloblin.w@gmail.com>
2024-05-19 15:01:02 -0300

    - Agrega correccion en error de filtrado

--- TPCarrito_Equipo_29/Default.aspx.designer.cs
+++ TPCarrito_Equipo_29/Default.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace TPCarrito_Equipo_29
     {
 
         /// <summary>
-        /// Control TextBox1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
         /// Control txtBuscar.
         /// </summary>
         /// <remarks>
